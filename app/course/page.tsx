73b21d3c96062cf4103aa769c045c2a7b35831b2
Lucas-Urbanski <lcurbanski8@gmail.com>
2026-03-26 12:53:48 -0600

    Settings

--- app/course/page.tsx
+++ app/course/page.tsx
@@ -1,9 +1,3 @@
-
 export default function Courses() {
-
-  return (
-    <div className="min-h-screen font-sans bg-[#F5F1E6]">
-      
-    </div>
-  );
+  return <div className="min-h-screen font-sans bg-[#F5F1E6]"></div>;
 }
