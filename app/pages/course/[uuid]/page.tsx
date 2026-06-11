aee42cfbc5ebed4276cf691665700dc8d8fe6b37
Lucas Urbanski <lcurbanski8@gmail.com>
2026-06-10 19:21:26 -0600

    visible student bio

--- app/pages/course/[uuid]/page.tsx
+++ app/pages/course/[uuid]/page.tsx
@@ -688,12 +688,14 @@ function CourseContent({ }: { onClose?: () => void }) {
                       </div>
                     )}
                     {s.fullName}
+                    {s.bio && (
                       <div className="flex flex-col gap-1">
                         <p className="text-[10px] font-bold uppercase opacity-50">
                           Bio
                         </p>
                         <p className="text-xs opacity-80"> {s.bio}</p>
                       </div>
+                    )}
                   </div>
                 ))}
               </div>
